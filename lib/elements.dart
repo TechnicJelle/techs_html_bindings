f911bf5200102c6b5f77af2ab89d3a0e5d68bdc0
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-03-21 23:31:52 +0100

    Add forgotten code export

--- lib/elements.dart
+++ lib/elements.dart
@@ -3,6 +3,7 @@ library;
 
 export "src/elements/base.dart";
 export "src/elements/body.dart";
+export "src/elements/code.dart";
 export "src/elements/data.dart";
 export "src/elements/head.dart";
 export "src/elements/headings.dart";
