892e0d122ba384c1c66c19c8ab9c4e91a6100387
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-04-25 01:58:00 +0200

    Add support for <hr> element

---
+++ lib/src/elements/separators.dart
@@ -0,0 +1,10 @@
+import "package:techs_html_bindings/elements.dart";
+
+class Hr extends Element {
+  Hr() : super(children: const Iterable.empty());
+
+  @override
+  String build() {
+    return "<hr>";
+  }
+}
