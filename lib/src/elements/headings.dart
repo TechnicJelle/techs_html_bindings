551fa21c2049ad854764605d11466eda338e6c99
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-04-30 02:25:20 +0200

    Moved some magic values to constant variables

--- lib/src/elements/headings.dart
+++ lib/src/elements/headings.dart
@@ -1,4 +1,4 @@
-// dart format width=131
+// dart format width=144
 import "package:techs_html_bindings/elements.dart";
 import "package:techs_html_bindings/utils.dart";
 
@@ -29,7 +29,7 @@ abstract class Hn extends Element {
   /// Create a Heading with the provided [level].
   ///
   /// If [autoLink] is null (or not provided), it will use the default options for each specific heading level:\
-  /// `H1`, `H5`, `H6` *do not* get auto-linked. `H2`, `H3`, `H4` *do* get auto-linked.
+  /// [H1], [H5], [H6] *do not* get auto-linked. [H2], [H3], [H4] *do* get auto-linked.
   factory Hn.fromLevel({
     required int level,
     required Iterable<Element> children,
@@ -39,12 +39,12 @@ abstract class Hn extends Element {
     bool autoID = true,
     bool? autoLink,
   }) => switch (level) {
-    1 => H1(children: children, id: id, classes: classes, inlineStyles: inlineStyles, autoID: autoID, autoLink: autoLink ?? false),
-    2 => H2(children: children, id: id, classes: classes, inlineStyles: inlineStyles, autoID: autoID, autoLink: autoLink ?? true),
-    3 => H3(children: children, id: id, classes: classes, inlineStyles: inlineStyles, autoID: autoID, autoLink: autoLink ?? true),
-    4 => H4(children: children, id: id, classes: classes, inlineStyles: inlineStyles, autoID: autoID, autoLink: autoLink ?? true),
-    5 => H5(children: children, id: id, classes: classes, inlineStyles: inlineStyles, autoID: autoID, autoLink: autoLink ?? false),
-    6 => H6(children: children, id: id, classes: classes, inlineStyles: inlineStyles, autoID: autoID, autoLink: autoLink ?? false),
+    1 => H1(children: children, id: id, classes: classes, inlineStyles: inlineStyles, autoID: autoID, autoLink: autoLink ?? H1.defaultAutoLink),
+    2 => H2(children: children, id: id, classes: classes, inlineStyles: inlineStyles, autoID: autoID, autoLink: autoLink ?? H2.defaultAutoLink),
+    3 => H3(children: children, id: id, classes: classes, inlineStyles: inlineStyles, autoID: autoID, autoLink: autoLink ?? H3.defaultAutoLink),
+    4 => H4(children: children, id: id, classes: classes, inlineStyles: inlineStyles, autoID: autoID, autoLink: autoLink ?? H4.defaultAutoLink),
+    5 => H5(children: children, id: id, classes: classes, inlineStyles: inlineStyles, autoID: autoID, autoLink: autoLink ?? H5.defaultAutoLink),
+    6 => H6(children: children, id: id, classes: classes, inlineStyles: inlineStyles, autoID: autoID, autoLink: autoLink ?? H6.defaultAutoLink),
     _ => throw Exception("Level $level was outside of the accepted range (1 to 6)"),
   };
 
@@ -66,49 +66,61 @@ abstract class Hn extends Element {
 }
 
 class H1 extends Hn {
-  H1({required super.children, super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = false})
+  static const bool defaultAutoLink = false;
+
+  H1({required super.children, super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = defaultAutoLink}) //
     : super(level: 1);
 
-  H1.text(String text, {super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = false})
+  H1.text(String text, {super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = defaultAutoLink})
     : super(level: 1, children: [T(text)]);
 }
 
 class H2 extends Hn {
-  H2({required super.children, super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = true})
+  static const bool defaultAutoLink = true;
+
+  H2({required super.children, super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = defaultAutoLink}) //
     : super(level: 2);
 
-  H2.text(String text, {super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = true})
+  H2.text(String text, {super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = defaultAutoLink})
     : super(level: 2, children: [T(text)]);
 }
 
 class H3 extends Hn {
-  H3({required super.children, super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = true})
+  static const bool defaultAutoLink = true;
+
+  H3({required super.children, super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = defaultAutoLink}) //
     : super(level: 3);
 
-  H3.text(String text, {super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = true})
+  H3.text(String text, {super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = defaultAutoLink})
     : super(level: 3, children: [T(text)]);
 }
 
 class H4 extends Hn {
-  H4({required super.children, super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = true})
+  static const bool defaultAutoLink = true;
+
+  H4({required super.children, super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = defaultAutoLink}) //
     : super(level: 4);
 
-  H4.text(String text, {super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = true})
+  H4.text(String text, {super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = defaultAutoLink})
     : super(level: 4, children: [T(text)]);
 }
 
 class H5 extends Hn {
-  H5({required super.children, super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = false})
+  static const bool defaultAutoLink = false;
+
+  H5({required super.children, super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = defaultAutoLink}) //
     : super(level: 5);
 
-  H5.text(String text, {super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = false})
+  H5.text(String text, {super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = defaultAutoLink})
     : super(level: 5, children: [T(text)]);
 }
 
 class H6 extends Hn {
-  H6({required super.children, super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = false})
+  static const bool defaultAutoLink = false;
+
+  H6({required super.children, super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = defaultAutoLink}) //
     : super(level: 6);
 
-  H6.text(String text, {super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = false})
+  H6.text(String text, {super.id, super.classes, super.inlineStyles, super.autoID, super.autoLink = defaultAutoLink})
     : super(level: 6, children: [T(text)]);
 }
